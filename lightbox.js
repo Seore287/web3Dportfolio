677fc49b08ca9a6a4cf9b45c1eac4e1dae3cbe17
Seore0102 <91267090+Seore287@users.noreply.github.com>
2025-03-29 02:28:39 +0000

    Updates

---
+++ lightbox.js
@@ -0,0 +1,8 @@
+function openLightbox(imageSrc) {
+    document.getElementById('lightbox').style.display = 'block';
+    document.getElementById('lightbox-img').src = imageSrc;
+}
+
+function closeLightbox() {
+    document.getElementById('lightbox').style.display = 'none';
+}
